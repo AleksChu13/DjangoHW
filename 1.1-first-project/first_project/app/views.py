67c854a794b0f98a1d22cd82a0a9ec38b8c85b14
Aleks <aleks.chudin2016@yandex.ru>
2022-04-20 20:41:18 +0300

    First _Project

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
+from os import scandir
+from django.conf import settings
 
 
 def home_view(request):
@@ -8,10 +11,10 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -21,15 +24,28 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    return HttpResponse(f'Work DIR content {settings.BASE_DIR} :<br>{make_dir_tree(settings.BASE_DIR)}')
+
+
+def make_dir_tree(dir_, lvl=''):
+    ignore_names = ['__pycache__']
+    result = ''
+    dir_content = sorted(scandir(dir_), key=lambda x: not x.is_dir())
+    for dc in dir_content:
+        if dc.is_dir(follow_symlinks=False) and dc.name not in ignore_names:
+            files = make_dir_tree(dc.path, lvl + '--')
+            result += lvl + '|_ ' + str(dc.name) + '<br>' + files
+        elif dc.name not in ignore_names:
+            result += lvl + '| ' + str(dc.name) + '<br>'
+    return result
