5c532b540712bad2c5db90e02c40a9e7a98134a4
Aleks <aleks.chudin2016@yandex.ru>
2022-05-23 15:00:51 +0300

    HW 1

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,43 +1,51 @@
+from pprint import pprint
+
+from django.db import models
 from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
-
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
+        for position in positions:
+            # Оставил комментом для себя как пример
+            # position['stock'] = stock
+            # position = (StockProduct(stock=position['stock'], product=position['product'],
+            #                          quantity=position['quantity'], price=position['price'])).save()
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+            StockProduct.objects.create(stock=stock, **position)
 
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
+        print(instance)
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=instance, product=position.get('product'), defaults=position)
 
         return stock
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
